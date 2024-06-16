c6b95cba7cf850fd109d9fcd8fd56cd6e4b1abfd
syrongk <syrongk@gmail.com>
2024-06-16 23:44:09 +0900

    build: cloud config client 추가

--- src/main/java/org/example/user/controller/UserController.java
+++ src/main/java/org/example/user/controller/UserController.java
@@ -1,11 +1,15 @@
 package org.example.user.controller;
 
 import org.example.core.wrapper.ResultResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/apis/user")
 public class UserController {
+
+    @Value("${test.property.vault}")
+    private String test;
 
     @GetMapping("/home")
     public ResultResponse<Void> home() {
