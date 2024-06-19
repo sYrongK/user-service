7d3d6dd901c63365d95939cb053deffc5c6aeacc
sYrongK <syrongk@gmail.com>
2024-06-19 13:47:19 +0900

    api 변경

--- src/main/java/org/example/user/controller/UserController.java
+++ src/main/java/org/example/user/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/apis/user")
+@RequestMapping("/apis")
 public class UserController {
 
     @Value("${test.property.value}")
